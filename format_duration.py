4808b5097d9c1f27fc8322d816b9738648684a26
witold <witoldz@therapieclinic.com>
2022-01-17 19:28:50 +0000

    print duration

---
+++ format_duration.py
@@ -0,0 +1,65 @@
+import unittest
+
+
+def print_duration(durations):
+    output = ''
+
+    for i, k in enumerate(durations):
+        time_chunk = f'{durations[k]} {k}'
+
+        if durations[k] > 1:
+            time_chunk += 's'
+
+        if i == (len(durations) - 2):
+            time_chunk += ' and '
+        elif i < (len(durations) - 2):
+            time_chunk += ', '
+
+        output += time_chunk
+    return output
+
+
+def format_duration(seconds):
+    if not seconds:
+        return 'now'
+
+    minute = 60
+    hour = 60 * minute
+    day = 24 * hour
+    year = 365 * day
+
+    durations = dict()
+
+    y = seconds // year
+    if y > 0:
+        durations['year'] = y
+    seconds -= (year * y)
+
+    d = seconds // day
+    if d > 0:
+        durations['day'] = d
+    seconds -= (day * d)
+
+    h = seconds // hour
+    if h > 0:
+        durations['hour'] = h
+    seconds -= (hour * h)
+
+    m = seconds // minute
+    if m > 0:
+        durations['minute'] = m
+    seconds -= (minute * m)
+
+    if seconds > 0:
+        durations['second'] = seconds
+
+    return print_duration(durations)
+
+
+class TestFormatDuration(unittest.TestCase):
+    def test_duration(self):
+        self.assertEqual(format_duration(1), "1 second")
+        self.assertEqual(format_duration(62), "1 minute and 2 seconds")
+        self.assertEqual(format_duration(120), "2 minutes")
+        self.assertEqual(format_duration(3600), "1 hour")
+        self.assertEqual(format_duration(3662), "1 hour, 1 minute and 2 seconds")
